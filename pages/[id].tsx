9386a4c35919a64ade2490e822918c557d3fd9b2
kanpann <45007556+kanpann@users.noreply.github.com>
2021-04-05 16:56:50 +0900

    대표 이미지 위에 제목 및 날짜 표출

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,23 +1,64 @@
+import React from 'react'
 import Head from 'next/head'
 import { getAllPostIds, getPostData, Post as PostType } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 import { Date, Header, Layout } from '../components'
-import React from 'react'
+import styled from 'styled-components'
+
+const TitleImage = styled.div`
+  position: relative;
+  background-image: ${(props) => `url(${props.color})` || ''};
+  height: 40vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: block;
+`
+const TitleImageCover = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 1;
+`
+const Content = styled.div`
+  position: absolute;
+  top: 35%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 5rem;
+  color: white;
+  z-index: 2;
+  text-align: center;
+`
+const DateText = styled.div`
+  font-size: 1.5rem;
+`
 
 type PostProps = {
   post: PostType
 }
 export default function Post({ post }: PostProps) {
   const { title, date, content, image } = post
+
   return (
     <>
       <Header />
       <Layout maxWidth="md">
-        <article>
+        <TitleImage color={image}>
+          <TitleImageCover />
+        </TitleImage>
+        <Content>
           <h1 className={utilStyles.headingXl}>{title}</h1>
-          <div className={utilStyles.lightText}>
+          <DateText>
             <Date date={date} />
-          </div>
+          </DateText>
+        </Content>
+        <article>
+          {/* <h1 className={utilStyles.headingXl}>{title}</h1> */}
+          {/* <div className={utilStyles.lightText}>
+            <Date date={date} />
+          </div> */}
           <div dangerouslySetInnerHTML={{ __html: content }} />
         </article>
       </Layout>
