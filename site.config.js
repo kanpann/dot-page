a66828daf200366b7c9348efdbcd5f5db6cf3745
kanpann <45007556+kanpann@users.noreply.github.com>
2021-04-08 16:11:27 +0900

    블로그 설정 정보 js로 관리

---
+++ site.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  SiteMeta: {
+    title: 'Gunlog',
+    author: 'Gunkim',
+    github: 'https://github.com/kanpann',
+    profileImage: 'https://avatars.githubusercontent.com/u/45007556?v=4',
+    email: 'gunkim0318@gmail.com',
+  },
+  Category: {
+    개발: {
+      url: '/category?category=?',
+      isSub: true,
+      sub: {
+        Linux: '/category?category=?',
+        Spring: '/category?category=?',
+        JavaScript: '/category?category=?',
+        React: '/category?category=?',
+        Vue: '/category?category=?',
+        Kotlin: '/category?category=?',
+        'Data Structure': '/category?category=?',
+        'Error Log': '/category?category=?',
+        Python: '/category?category=?',
+        Java: '/category?category=?',
+        Other: '/category?category=?',
+      },
+    },
+    생각: {
+      url: '/category?category=Think',
+      isSub: false,
+    },
+  },
+}
