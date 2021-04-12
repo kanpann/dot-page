8f220b7247fdffaea969acd7e12f35cba1a6e560
kanpann <45007556+kanpann@users.noreply.github.com>
2021-04-12 13:33:21 +0900

    배경색 수정

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: 'white',
+    },
+  }),
+)
 
 type LayoutProps = {
   children: JSX.Element[] | JSX.Element
   maxWidth?: false | 'lg' | 'xs' | 'sm' | 'md' | 'xl' | undefined
 }
 export const Layout = ({ children, maxWidth = 'lg' }: LayoutProps) => {
+  const classes = useStyles()
   return (
-    <Container fixed maxWidth={maxWidth} style={{ paddingTop: '20px', paddingBottom: '80px' }}>
+    <Container
+      className={classes.root}
+      fixed
+      maxWidth={maxWidth}
+      style={{ paddingTop: '20px', paddingBottom: '80px' }}
+    >
       {children}
     </Container>
   )
