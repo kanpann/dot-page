58da662107bfaf708c67129e1c77f47f61ebe45b
kanpann <45007556+kanpann@users.noreply.github.com>
2021-04-17 02:32:44 +0900

    게시글 Card로 감싸서 게시글별 구분이 가도록 수정

--- components/post/Post.tsx
+++ components/post/Post.tsx
@@ -3,7 +3,7 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import DateView from './DateView'
-import { Chip } from '@material-ui/core'
+import { Card, Chip } from '@material-ui/core'
 import styled from 'styled-components'
 
 const TagFrame = styled.div`
@@ -21,37 +21,44 @@ type PostProps = {
 export const Post = ({ title, date, image, excerpt, tags }: PostProps) => {
   return (
     <>
-      <CardMedia
-        component="img"
-        alt="Contemplative Reptile"
-        height="200"
-        image={image}
-        title="Contemplative Reptile"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2" style={{ fontFamily: 'nanumSquare' }}>
-          {title}
-        </Typography>
-        <TagFrame>
-          {tags.map((tag) => (
-            <Chip
-              key={tag}
-              style={{ marginRight: '5px' }}
-              label={tag}
-              component="a"
-              href="#chip"
-              clickable
-              color="primary"
-            />
-          ))}
-        </TagFrame>
-        <span>
-          <DateView date={date} />
-        </span>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {excerpt}
-        </Typography>
-      </CardContent>
+      <Card>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="200"
+          image={image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="h2"
+            style={{ fontFamily: 'nanumSquare' }}
+          >
+            {title}
+          </Typography>
+          <TagFrame>
+            {tags.map((tag) => (
+              <Chip
+                key={tag}
+                style={{ marginRight: '5px' }}
+                label={tag}
+                component="a"
+                href="#chip"
+                clickable
+                color="primary"
+              />
+            ))}
+          </TagFrame>
+          <span>
+            <DateView date={date} />
+          </span>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {excerpt}
+          </Typography>
+        </CardContent>
+      </Card>
     </>
   )
 }
