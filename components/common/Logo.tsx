292b480f7a0886131d414d85d5ee118e6bfc0441
kanpann <45007556+kanpann@users.noreply.github.com>
2021-04-24 19:32:54 +0900

    로고를 위한 폰트 추가 및 적용

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -2,17 +2,27 @@ import Link from 'next/link'
 import React from 'react'
 import styled from 'styled-components'
 
-const CustomLogo = styled.img`
+const CustomLogo = styled.div`
   margin: 0 auto;
   margin-top: 10px;
   margin-bottom: 100px;
   cursor: pointer;
+  font-family: 'sunspots';
+  font-size: 6rem;
+  text-align: center;
+  padding: 50px 10px;
+  color: ${(props) => props.theme.color.title};
+  -ms-user-select: none;
+  -moz-user-select: -moz-none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  user-select: none;
 `
 
 const Logo = () => {
   return (
     <Link href="/">
-      <CustomLogo src="/images/logo.png" />
+      <CustomLogo>Gunlog</CustomLogo>
     </Link>
   )
 }
