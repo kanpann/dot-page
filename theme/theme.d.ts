28d9fbc4effe707ad1cb539c37d2176a9ea259c1
kanpann <gunkim.dev@gmail.com>
2021-04-30 10:27:16 +0900

    styled 타입 파일 통합

--- theme/theme.d.ts
+++ theme/theme.d.ts
@@ -3,4 +3,10 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {
   }
+  export interface PostHeaderTheme {
+    image: string;
+  }
+  export interface GlobalTheme {
+    primary: string;
+  }
 }
