6a12f484b94b9313890be91a619b9e487aa6c5fd
gunkims <gunkim.dev@gmail.com>
2021-05-04 17:06:21 +0900

    검색엔진 최적화를 위해 helmet 적용

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,4 +1,15 @@
+import { Helmet } from 'react-helmet'
+import { SiteMeta } from '../../site.config'
+
 const Header = () => {
-  return <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png" />
+  return (
+    <>
+      <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png" />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{SiteMeta.title}</title>
+      </Helmet>
+    </>
+  )
 }
 export default Header
