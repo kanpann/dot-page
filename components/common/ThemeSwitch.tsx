89ee911a0bf589ede084a4c96638e370c0ba4f54
gunkims <gunkim.dev@gmail.com>
2021-05-06 01:15:10 +0900

    라이트, 다크 아이콘 변경

--- components/common/ThemeSwitch.tsx
+++ components/common/ThemeSwitch.tsx
@@ -1,28 +1,26 @@
 import { withTheme } from '@material-ui/core'
 import { styled as muiStyled } from '@material-ui/core/styles'
-import SunIcon from '@material-ui/icons/BrightnessLow'
-import MoonIcon from '@material-ui/icons/Brightness2'
 import IconButton from '@material-ui/core/IconButton'
 import { DefaultTheme } from '../../theme/Theme'
 import React from 'react'
 import { ThemeCtxConsumer } from '../provider/ThemeCtxProvider'
+import Brightness7Icon from '@material-ui/icons/Brightness7'
+import Brightness4Icon from '@material-ui/icons/Brightness4'
 
 const MyThemeIcon = muiStyled(withTheme(IconButton))((props: DefaultTheme) => ({
   float: 'right',
   color: props.theme.app.title,
-  paddingLeft: '10px'
+  paddingLeft: '10px',
 }))
 
 const ThemeSwitch = () => {
   return (
     <ThemeCtxConsumer>
-    {
-      ({ theme, fn }) => (
+      {({ theme, fn }) => (
         <MyThemeIcon onClick={fn} aria-label="display more actions" edge="end" color="inherit">
-          {theme=='light'? <SunIcon/>:<MoonIcon/>}
+          {theme == 'light' ? <Brightness7Icon /> : <Brightness4Icon />}
         </MyThemeIcon>
-      )
-    }
+      )}
     </ThemeCtxConsumer>
   )
 }
