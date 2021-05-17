b8b3ad0def64a60599f37a6221d19bd762442d8b
gunkims <gunkim.dev@gmail.com>
2021-05-17 14:39:23 +0900

    빌드 오류 수정

--- pages/404.tsx
+++ pages/404.tsx
@@ -12,7 +12,7 @@ const NotFoundMsg = muiStyled(withTheme(Typography))((props: DefaultTheme) => ({
 const ContentsMsg = muiStyled(withTheme(Typography))((props: DefaultTheme) => ({
   color: props.theme.app.title,
   fontFamily: 'sunspots',
-  margin: '50px 0px',
+  margin: '25px 0px',
 }))
 export default function NotFound() {
   return (
@@ -26,13 +26,13 @@ export default function NotFound() {
       <ContentsMsg align="center">
         찾을 수 없는 페이지입니다. 요청하신 페이지가 사라졌거나, 잘못된 경로를 이용하셨어요.
       </ContentsMsg>
-      <div align="center">
+      <ContentsMsg style={{ textAlign: 'center' }}>
         <Link href="/">
           <Button variant="outlined" color="primary">
             홈으로
           </Button>
         </Link>
-      </div>
+      </ContentsMsg>
     </Layout>
   )
 }
