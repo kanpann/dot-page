0018f15db2d4ebe286171133f508eab8297f42d2
kanpann <gunkim.dev@gmail.com>
2021-05-17 16:51:56 +0900

    사이트 설정 파일 수정

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
     profileImage: 'https://avatars.githubusercontent.com/u/45007556?v=4',
     info: {
       author: 'Gunkim',
-      descript: '안녕하세요. Gunkim입니다. 공부한 내용들을 정제하기 위해서 블로그를 시작했습니다.',
+      descript: '안녕하세요. 공부한 내용들을 정제하기 위해서 블로그를 시작했습니다.',
       github: 'https://github.com/gunkims',
       email: 'gunkim.dev@gmail.com',
     },
@@ -16,14 +16,11 @@ module.exports = {
       clientID: '950a9f1473b04652cbc0',
       clientSecret: '6b42fe4d369ba3a4918c0b629b35115111fb33ac',
       repo: 'blog-gitalk',
-      owner: 'kanpann',
-      admin: ['kanpann'],
+      owner: 'gunkims',
+      admin: ['gunkims'],
     },
   },
   Category: {
-    개발환경: {
-      isSub: false,
-    },
     Java: {
       isSub: true,
       sub: ['Spring'],
@@ -39,8 +36,17 @@ module.exports = {
       'https://user-images.githubusercontent.com/45007556/99826279-7c48c080-2b9b-11eb-8cce-3c92f971c803.png',
       '자바스크립트에 대한 글들을 모아놓은 카테고리입니다.',
     ),
-    React: new Category('', ''),
-    Vue: new Category('', ''),
-    Java: new Category('', ''),
+    React: new Category(
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7qTq2qvMKCqK9ymWIyyXIDmGglzjJImp2BA&usqp=CAU',
+      '리액트에 대한 글들을 모아놓은 카테고리입니다.',
+    ),
+    Vue: new Category(
+      'https://user-images.githubusercontent.com/45007556/111740934-f6c6ea00-88c8-11eb-8a49-c1f112655c81.png',
+      '뷰에 대한 글들을 모아놓은 카테고리입니다.',
+    ),
+    Java: new Category(
+      'https://media.vlpt.us/images/ym1085/post/67c7a3b2-3d80-4d40-887a-a4f500772e76/java.png',
+      '자바에 대한 글들을 모아놓은 카테고리입니다.',
+    ),
   },
 }
