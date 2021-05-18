873b83524d35f0a59771e1c6a2de71ea4cb6324c
gunkims <gunkim.dev@gmail.com>
2021-05-18 15:38:09 +0900

    검색, 태그에 대한 title 변경되도록 수정

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/dist/client/router'
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import ScrollProgressBar from './ScrollProgressBar'
@@ -6,6 +7,15 @@ type HeaderProps = {
   title: string
 }
 const Header = ({ title }: HeaderProps) => {
+  const router = useRouter()
+  const tag = router.query.tag as string
+  const search = router.query.keyword as string
+  if (tag) {
+    title = `'${tag}' 태그의 글 목록`
+  }
+  if (search) {
+    title = `'${search}'의 검색결과`
+  }
   return (
     <>
       <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png" />
