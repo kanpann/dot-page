d790b6708d7cfce56bf564758b2fa61f8a59dd69
gunkims <gunkim.dev@gmail.com>
2021-05-19 23:19:41 +0900

    불필요한 임포트 제거

--- components/common/Categories.tsx
+++ components/common/Categories.tsx
@@ -1,6 +1,6 @@
 // 카테고리 정보를 밖에서 주입 받게 수정하면 이 컴포넌트를 재사용할 수 있을 것 같음. 나중에 고려해봄
 import { Category } from '../../site.config'
-import styled, { CategoryTheme } from 'styled-components'
+import styled from 'styled-components'
 import Link from 'next/link'
 import { DefaultTheme } from '../../theme/Theme'
 import { styled as muiStyled } from '@material-ui/core/styles'
