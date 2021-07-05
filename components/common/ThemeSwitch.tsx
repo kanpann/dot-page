f7b1de901d9112e5f9100a6c4dc8ff1c5bb78458
gunkims <gunkim.dev@gmail.com>
2021-07-05 15:56:44 +0900

    다크<>라이트 서로 변경 및 css 조금 수정

--- components/common/ThemeSwitch.tsx
+++ components/common/ThemeSwitch.tsx
@@ -6,16 +6,16 @@ import { DefaultTheme } from '../../theme/Theme'
 import { ThemeCtxConsumer } from '../provider/ThemeCtxProvider'
 import Brightness3Icon from '@material-ui/icons/Brightness3'
 import WbSunnyIcon from '@material-ui/icons/WbSunny'
-import TopScrollBtn from '../common/TopScrollBtn'
+import TopScrollBtn from './TopScrollBtn'
 
 const MyThemeIcon = muiStyled(withTheme(IconButton))((props: DefaultTheme) => ({
-  color: props.theme.app.title,
+  color: 'black',
   position: 'fixed',
   bottom: 10,
   right: 20,
-  background: props.theme.app.box,
-  borderRadius: '10px',
-  border: `2px solid ${props.theme.app.title}`,
+  background: '#f5ffe9',
+  borderRadius: '8px',
+  border: `2px solid black`,
   fontSize: '1.2rem',
   padding: '7px',
 }))
@@ -28,11 +28,11 @@ const ThemeSwitch = () => {
           <section onClick={fn}>
             {theme == 'light' ? (
               <>
-                <WbSunnyIcon viewBox="0 -6 28 28" /> <span>Light 테마</span>
+                <Brightness3Icon viewBox="0 -6 28 28" /> <span>Dark 테마</span>
               </>
             ) : (
               <>
-                <Brightness3Icon viewBox="0 -6 28 28" /> <span>Dark 테마</span>
+                <WbSunnyIcon viewBox="0 -6 28 28" /> <span>Light 테마</span>
               </>
             )}
           </section>{' '}
