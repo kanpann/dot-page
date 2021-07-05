6397b73109e69658bd5bfec4c82e1e0c3b660f9d
gunkims <gunkim.dev@gmail.com>
2021-07-05 15:54:46 +0900

    로고 제거, 여백 조건 추가

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -7,10 +7,9 @@ import Logo from '../common/Logo'
 import SideMenuBar from '../sidebar/SideMenuBar'
 import { DefaultTheme } from '../../theme/Theme'
 import Footer from './Footer'
-import ThemeSwitch from '../header/ThemeSwitch'
+import ThemeSwitch from './ThemeSwitch'
 import { styled as muiStyled } from '@material-ui/core/styles'
 import Header from '../header/Header'
-import TopScrollBtn from './TopScrollBtn'
 
 const MyMenuIcon = muiStyled(withTheme(MenuIcon))((props: DefaultTheme) => ({
   color: props.theme.app.title,
@@ -21,7 +20,7 @@ const Frame = styled.div`
   padding: 20px 20px;
   padding-bottom: 20px;
   margin-bottom: 20px;
-  border-radius: 15px;
+  border-radius: 7px;
 `
 type LayoutProps = {
   children: JSX.Element[] | JSX.Element
@@ -49,7 +48,7 @@ const Layout = ({ children, maxWidth = 'lg' }: LayoutProps) => {
           </IconButton>
         </Hidden>
         <Drawer open={isOpenMenu} anchor="left" onClose={handleMenuPoper}>
-          <SideMenuBar isLogo={true} isBorder={true} handleClose={handleMenuPoper} />
+          <SideMenuBar whiteSpace="large" isBorder={true} handleClose={handleMenuPoper} />
         </Drawer>
         <Grid container direction="row" justify="center" alignItems="flex-start">
           <Hidden smDown>
